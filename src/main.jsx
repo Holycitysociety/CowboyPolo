5510e6c08a43f14dc0a691a67efc19eed8d9bc2c
Holycitysociety <99046407+Holycitysociety@users.noreply.github.com>
2026-01-01 20:06:04 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { ThirdwebProvider } from "thirdweb/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* Same clientId + provider style as Patron site */}
+    {/* Same clientId + provider setup as Patron site */}
     <ThirdwebProvider clientId="f58c0bfc6e6a2c00092cc3c35db1eed8">
       <App />
     </ThirdwebProvider>
